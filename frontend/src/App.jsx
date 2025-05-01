14ec922098114f31ce703ec4c95569159434a835
Guddu Kumar <guddu748081@gmail.com>
2025-05-01 18:10:12 +0530

    taileind css setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,18 +10,18 @@ function App() {
       </h2>
 
       <div>
-  <div class="bg-sky-50">rahul kumar</div>
-  <div class="bg-sky-100">rahul kumar</div>
-  <div class="bg-sky-200">rahul kumar</div>
-  <div class="bg-sky-300">rahul kumar</div>
-  <div class="bg-sky-400">rahul kumar</div>
-  <div class="bg-sky-500">rahul kumar</div>
-  <div class="bg-sky-600">rahul kumar</div>
-  <div class="bg-sky-700">rahul kumar</div>
-  <div class="bg-sky-800">rahul kumar</div>
-  <div class="bg-sky-900">rahul kumar</div>
-  <div class="bg-sky-950">rahul kumar</div>
-</div>
+        <div class="bg-sky-300">rahul kumar</div>
+        <div class="bg-sky-100">rahul kumar</div>
+        <div class="bg-sky-200">rahul kumar</div>
+        <div class="bg-sky-300">rahul kumar</div>
+        <div class="bg-sky-400">rahul kumar</div>
+        <div class="bg-sky-500">rahul kumar</div>
+        <div class="bg-sky-600">rahul kumar</div>
+        <div class="bg-sky-700">rahul kumar</div>
+        <div class="bg-sky-800">rahul kumar</div>
+        <div class="bg-sky-900">rahul kumar</div>
+        <div class="bg-sky-950">rahul kumar</div>
+      </div>
     </>
   )
 }
