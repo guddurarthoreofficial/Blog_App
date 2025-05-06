74ec3184890f0fc8fa98797c97731d32d326f76a
Guddu Kumar <guddu748081@gmail.com>
2025-05-06 11:37:29 +0530

    All frontend pages set with Basic Code.

---
+++ frontend/src/dashboard/CreareBlog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreareBlog = () => {
+  return (
+    <div>CreareBlog</div>
+  )
+}
+
+export default CreareBlog
---
+++ frontend/src/pages/Creaters.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Creaters = () => {
+  return (
+    <div>Creaters</div>
+  )
+}
+
+export default Creaters
