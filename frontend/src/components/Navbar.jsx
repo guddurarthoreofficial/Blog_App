ce8a5fa0184b63b270363ab96f7e6fb749b9da6e
Guddu Kumar <guddu748081@gmail.com>
2025-05-09 12:54:08 +0530

    navba toggle button is added with basic functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+
+
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
     <>
-
       <nav className='shadow-lg px-4 py-2'>
         <div className='flex justify-between items-center container mx-auto '>
           {/* ================ logo ============= */}
@@ -12,9 +16,35 @@ const Navbar = () => {
             Cilli<span className='text-blue-500'>Blog</span>
           </div>
 
-          {/*================= nav menu ================ */}
-          <div>
-            <ul className='flex space-x-6'>
+          {/*================= Destop ================ */}
+          <div className=''>
+            <ul className='space-x-6 hidden md:flex'>
+              <Link to="/" className=' hover:text-blue-500'>HOME</Link>
+              <Link to="/blogs" className=' hover:text-blue-500'>BLOG</Link>
+              <Link to="/crators" className=' hover:text-blue-500'>CREATORS</Link>
+              <Link to="/about" className=' hover:text-blue-500'>ABOUT</Link>
+              <Link to="/contact" className=' hover:text-blue-500'>CONTACT</Link>
+            </ul>
+
+            <div className='md:hidden' onClick={() => setShow(!show)}>
+              {show ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </div>
+          </div>
+
+          {/* // ======== login button ====== */}
+
+          <div className='space-x-2'>
+            <Link to="/dashboard" className='bg-blue-600 text-white font font-semibold hover:bg-blue-800 duration-300 px-4 py-2 rounded hidden md:flex' >DASHBOARD</Link>
+            <Link to="/Login" className='bg-red-600 text-white font font-semibold hover:bg-red-800 duration-300 px-4 py-2 rounded hidden md:flex' >LOGIN </Link>
+          </div>
+
+
+        </div>
+
+        {/*  =================== Mobile navbaar============  */}
+        {show && (
+          <div className='bg-white'>
+            <ul className='flex  md:hidden h-screen items-center text-xl  flex-col  space-y-3'>
               <Link to="/" className=' hover:text-blue-500'>HOME</Link>
               <Link to="/blogs" className=' hover:text-blue-500'>BLOG</Link>
               <Link to="/crators" className=' hover:text-blue-500'>CREATORS</Link>
@@ -22,17 +52,9 @@ const Navbar = () => {
               <Link to="/contact" className=' hover:text-blue-500'>CONTACT</Link>
             </ul>
           </div>
-
-          {/* // ======== login button ====== */}
-
-          <div className='space-x-2'>
-            <Link to="/dashboard" className='bg-blue-600 text-white font font-semibold hover:bg-blue-800 duration-300 px-4 py-2 rounded ' >DASHBOARD</Link>
-            <Link to="/Login" className='bg-red-600 text-white font font-semibold hover:bg-red-800 duration-300 px-4 py-2 rounded ' >LOGIN </Link>
-          </div>
+        )}
 
 
-          
-        </div>
       </nav>
 
     </>
