7ca9dbd9f4a65f273fbedece4c85f295b88fbda3
Guddu Kumar <guddu748081@gmail.com>
2025-05-11 10:54:35 +0530

    Home Devetional Blog component is added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import Hero from '../Home/Hero'
 import Trending from '../Home/Trending'
+import Devotional from '../Home/Devotional'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <Trending/>
+      <Devotional/>
     </>
   )
 }
